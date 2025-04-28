098ec39a0590bebb23b3f698c72e9125eed4ee32
Yassmine Hnainia <yassminehnainia1@gmail.com>
2025-04-28 14:28:35 +0100

    Simplify .env loading in TwilioSMSUtil using Properties, fix typo in comment

--- src/main/java/tn/esprit/utils/TwilioSMSUtil.java
+++ src/main/java/tn/esprit/utils/TwilioSMSUtil.java
@@ -3,21 +3,36 @@ package tn.esprit.utils;
 import com.twilio.Twilio;
 import com.twilio.rest.api.v2010.account.Message;
 import com.twilio.type.PhoneNumber;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
 
 public class TwilioSMSUtil {
 
-
-    public static final String ACCOUNT_SID = "AC7556b2670120201b8dd7d36e1ff06571";
-    public static final String AUTH_TOKEN = "ebd69f977fb85ef1026507e1f39bb23e";
-    public static final String TWILIO_PHONE_NUMBER = "\t\n" +
-            "+16084532249";
+    public static final String ACCOUNT_SID;
+    public static final String AUTH_TOKEN;
+    public static final String TWILIO_PHONE_NUMBER;
 
     static {
+        Properties props = new Properties();
+        try (FileInputStream fis = new FileInputStream(".env")) {
+            props.load(fis);
+        } catch (IOException e) {
+            throw new IllegalStateException("❌ Failed to load .env file: " + e.getMessage());
+        }
+
+        ACCOUNT_SID = props.getProperty("TWILIO_ACCOUNT_SID");
+        AUTH_TOKEN = props.getProperty("TWILIO_AUTH_TOKEN");
+        TWILIO_PHONE_NUMBER = props.getProperty("TWILIO_PHONE_NUMBER");
+
+        if (ACCOUNT_SID == null || AUTH_TOKEN == null || TWILIO_PHONE_NUMBER == null) {
+            throw new IllegalStateException("❌ Twilio credentials are missing in .env file");
+        }
         Twilio.init(ACCOUNT_SID, AUTH_TOKEN);
     }
 
     /**
-     * Envoie un SMS via Twilio
+     * Envoi un SMS via Twilio
      * @param toPhoneNumber Numéro du destinataire (format international, ex: +216...)
      * @param messageBody Contenu du SMS
      */
