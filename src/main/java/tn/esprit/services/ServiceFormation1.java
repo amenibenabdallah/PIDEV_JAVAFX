9c80988002c664606d2256640cab48f8371cdecf
hazem lachheb <lachhebhazem87@gmail.com>
2025-05-03 21:10:59 +0100

    renomme le ficher formation et serviceformation

--- src/main/java/tn/esprit/services/ServiceFormation1.java
+++ src/main/java/tn/esprit/services/ServiceFormation1.java
@@ -1,18 +1,18 @@
 package tn.esprit.services;
 
-import tn.esprit.models.Formation;
+import tn.esprit.models.Formation1;
 
 import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
 
-public class ServiceFormation {
+public class ServiceFormation1 {
     private static final String URL = "jdbc:mysql://localhost:3306/formini111";
     private static final String USER = "root";
     private static final String PASSWORD = "";
 
-    public List<Formation> getIdAndTitre() {
-        List<Formation> formations = new ArrayList<>();
+    public List<Formation1> getIdAndTitre() {
+        List<Formation1> formations = new ArrayList<>();
         String query = "SELECT id, titre FROM formation";
 
         try (Connection conn = DriverManager.getConnection(URL, USER, PASSWORD);
@@ -20,7 +20,7 @@ public class ServiceFormation {
              ResultSet rs = stmt.executeQuery(query)) {
 
             while (rs.next()) {
-                Formation formation = new Formation();
+                Formation1 formation = new Formation1();
                 formation.setId(rs.getInt("id"));
                 formation.setTitre(rs.getString("titre"));
                 formations.add(formation);
